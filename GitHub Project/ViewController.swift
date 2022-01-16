adbf55295f37aec5916f7de3a893da71dbe9c240
Sulaymon <karimovsulaymon055@gmail.com>
2022-01-16 15:33:44 +0500

    Initial Commit

---
+++ GitHub Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Project
+//
+//  Created by Sulaymon Karimov on 1/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
